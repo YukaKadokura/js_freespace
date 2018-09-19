dc7dc96c81882e21bfcd0b9884781013ba73f1e3
YukaKadokura <kadokura00000@gmail.com>
2018-09-19 22:57:42 +0900

    webpack.config.jsの作成。

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    context: __dirname + 'source',
+    entry:{
+        'application':'./es2015/application'
+    },
+    output:{
+        path:__dirname + '/distribution/javascript',
+        filename:'[name].js'
+    },
+    module:{
+        loaders:[
+            {
+                test:/¥.js/,
+                exclude:/node_modules/,
+                loader:"babel",
+                query:{
+                    presets:['es2015']
+                }
+            }
+        ]
+    }
+}
